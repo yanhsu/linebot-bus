ec0c4c1eb715dc02eccea30b787806c4f06a8872
yanhsu <e31217@gmail.com>
2019-01-13 00:01:20 +0800

    fix bus stopStatus

--- util/common.js
+++ util/common.js
@@ -49,11 +49,15 @@ module.exports.formatEstimatedTimeOfArrival = (estimatedTimeOfArrival) => {
     min = EstimateTime/60;
   }
   let nextTime = moment(NextBusTime).tz("Asia/Taipei").format("HH:mm");
-  if(PlateNumb == -1 || !PlateNumb) {
-    return `尚未發車\n下班車預計抵達時間${nextTime}`
-  } else if(StopStatus != 0) {
+  if(StopStatus == 2) {
+    return "交管不停靠"
+  } else if(StopStatus == 3) {
+    return "末班車已駛離"
+  } else if(StopStatus == 4) {
     return "尚未發車"
-  } else {
+  } else if(PlateNumb == -1 || !PlateNumb) {
+    return `尚未發車\n下班車預計抵達時間${nextTime}`
+  }  else {
     return `車號 ${PlateNumb}\n 將在${min}分鐘後抵達`
   }
 }
