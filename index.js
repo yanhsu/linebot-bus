b6e52ddf7a4ff83547c5a6c51b6282f8be21a227
shan <e31217@gmail.com>
2019-08-22 22:34:17 +0800

    調整回傳內容

--- index.js
+++ index.js
@@ -305,13 +305,10 @@ bot.on('message', async function(event) {
  cron.schedule('*/1 * * * *', async () => {
   const timeNow = moment().tz("Asia/Taipei").format("HH:mm");
   const favorites = await favoriteService.findByTriggerTime(timeNow);
-  console.log("test %O", await favoriteService.findByUserId(1));
-  console.log("timenow =>"+ timeNow + (timeNow == "20:21"));
-  console.log("favorites =>" + favorites);
   for(let favorite of favorites) {
     let res = await bus.getEstimateTimeByStopId(favorite.routeId, favorite.direction, favorite.stopId);
     const msg = formatEstimatedTimeOfArrival(res.data[0]);
-    bot.push(favorite.User.lineId, msg);
+    bot.push(favorite.User.lineId, `${favorite.routeId}路公車 \n${res.data[0].StopName.Zh_tw}站 ${msg}`);
   }
 
   console.log('running on every minute');
