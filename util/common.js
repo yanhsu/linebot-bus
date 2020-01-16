abc1a94124c15c7d1e1c5e0e2650511ee4374f01
yanhsu <e31217@gmail.com>
2020-01-16 16:28:22 +0800

    for test

--- util/common.js
+++ util/common.js
@@ -171,7 +171,7 @@ module.exports.formatBusFlexMessage = (routeName, stops) => {
     "contents": null
   }
   for(let [i,stop] of stops.entries()) {
-    if( i < 9) {
+    if( i < 11) {
       template.body.contents.push(
         {
           "type": "box",
