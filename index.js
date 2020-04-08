903ae24d80aa2bedd6b81f997258206baffebed6
yanhsu <e31217@gmail.com>
2020-04-08 09:59:19 +0800

    fix bug

--- index.js
+++ index.js
@@ -212,7 +212,7 @@ bot.on('message', async function(event) {
         for(let i = 1; i <= favorites.length; i++) {
           const favorite = favorites[i-1];
           let routeInfo = myCache.get(favorite.routeId);
-          deleteFavorites.push({ index: i, content: `${favorite.routeId} ${favorite.direction?`往${routeInfo.destinationStopName}`:`往${routeInfo.departureStopName}` } ${favorite.stopName}`})
+          deleteFavorites.push({ index: i, content: `${favorite.routeId} ${favorite.direction?`往${routeInfo.departureStopName}`:`往${routeInfo.destinationStopName}` } ${favorite.stopName}`})
           deleteMsg += `${i}. ${favorite.routeId} ${favorite.direction?`往${routeInfo.departureStopName}`:`往${routeInfo.destinationStopName}` } ${favorite.stopName}\n`;
         }
         deleteMsg += '\n0.取消';
